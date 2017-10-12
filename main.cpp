30ece44772184ce4b3543845a72dec701d267d0e
Rahul Kumar Parihar <31583515+RahulKumarParihar@users.noreply.github.com>
2017-10-12 19:14:22 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+extern int yyparse();
+
+int main()
+{
+	int result= yyparse();
+	if(result)
+		std::cout << "The input is valid." << std::endl;
+	else
+		std::cout << "The input is invalid." << std::endl;
+	return result;
+}
